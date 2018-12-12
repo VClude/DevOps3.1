0c68fc76bd5104727d54f18de00ce282aadd6646
VClude <vclude@gmail.com>
2018-12-12 14:34:11 +0700

    this is third commit

--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@
 public class Sample{
     public static void main(String args[]){
         System.out.println("This is just a sample java file for Git");
+        System.out.println("we are adding another output for see the changes");
+        System.out.println("This is second modification in java file to see the staged and unstaged changes");
     }
 }
 /* code ends */
