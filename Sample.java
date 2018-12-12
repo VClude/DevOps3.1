9fafeba3c1d389f04cbda1595b870d961a51a65f
VClude <vclude@gmail.com>
2018-12-12 14:32:25 +0700

    This is my second commit

---
+++ Sample.java
@@ -0,0 +1,7 @@
+/* code hajimaru */
+public class Sample{
+    public static void main(String args[]){
+        System.out.println("This is just a sample java file for Git");
+    }
+}
+/* code ends */
